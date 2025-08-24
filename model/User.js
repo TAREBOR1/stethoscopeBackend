7fc1267d1e840acab3d14ef7d316d216d2d45e99
tarebor <onodaifetareborgift@gmail.com>
2025-08-24 08:19:53 +0100

    updated backend

--- model/User.js
+++ model/User.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
   fullName: { type: String, required: true },
-  matricNumber: { type: String, unique: true, required: true },
+  matricNumber: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   otp: String,
   otpExpiry: Date,
